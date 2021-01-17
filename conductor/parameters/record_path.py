ac85ec5c49a3371672a0d77bceb95bc721b437ce
sr3srgang <sr3srgang@gmail.com>
2021-01-17 11:27:38 -0700

    enabled align

--- conductor/parameters/record_path.py
+++ conductor/parameters/record_path.py
@@ -1,16 +1,17 @@
 import numpy as np
 import os
-
 from conductor.parameter import ConductorParameter
+import time
 
 class SetRecordPath(ConductorParameter):
     autostart = True
     priority = 2
     record_keyword = 'savePictures'
+    align_keyword = 'align'
     data_filename = '{}_mako.png'
     camera_data_path = "K:/data/data"
     data_path = os.path.join(os.getenv('PROJECT_DATA_PATH'), 'data')
-    #record_sequence = {'blue_TOF': 'g', }
+    align_name = 'align.png'
 
     def initialize(self, config):
         self.connect_to_labrad()
@@ -27,10 +28,19 @@ class SetRecordPath(ConductorParameter):
                 #check if directory already exists; if not, make it
                 experiment_directory = os.path.join(self.data_path, experiment_name)
                 if not os.path.isdir(experiment_directory):
-                    os.makedirs(experiment_directory)
-                #test = 'K:/data/data/20210116/blue_TOF_savePictures#24/0_mako.png' 
+                    os.makedirs(experiment_directory) 
                 self.cxn.zuko_camera.get_frame(rel_point_path)
-
+ 
+        elif sequence is not None:
+            if self.align_keyword in str(sequence.value):
+                time_string = time.strftime('%Y%m%d')
+                today_path = os.path.join(self.camera_data_path, time_string, self.align_name)
+               
+               #check if today's data folder already exists; if not, make it
+                today_data_dir = os.path.join(self.data_path, time_string)
+                if not os.path.isdir(today_data_dir):
+                    os.makedirs(today_data_dir)
+                self.cxn.zuko_camera.get_frame(today_path)
 
 Parameter = SetRecordPath
 
