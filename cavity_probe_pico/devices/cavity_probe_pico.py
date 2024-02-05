810a789e14762e7d75b9c233caf1a54a94049cb1
srgang <sr3srgang@gmail.com>
2024-02-05 13:50:20 -0700

    default sr3 code

--- cavity_probe_pico/devices/cavity_probe_pico.py
+++ cavity_probe_pico/devices/cavity_probe_pico.py
@@ -7,30 +7,40 @@ class CavityPico(Picoscope):
 	#Properties for data recording:
 	picoscope_trigger_threshold = .1
 	picoscope_timeout = 5000# in ms
-	picoscope_duration =.52
-	picoscope_sampling_interval = 2e-6#2e-6#48e-9#16e-9#16e-9#16e-9#25e-6#16e-9
-	picoscope_n_capture = 1#3
+	picoscope_duration = .04#.52 #20230313 MM updated for multiple capture testing
+	picoscope_sampling_interval = 1e-6# 2e-6
+	#2e-6#48e-9#16e-9#16e-9#16e-9#25e-6#16e-9
+	picoscope_n_capture = 7
 #	picoscope_resolution = 12   #8, 12, 13, 14, 15, 16
 	picoscope_channel_settings = {
 		'A': {
 		    'coupling': 'DC',
-		    'VRange': .2,
+		    'VRange': 1,
 		    'probeAttenuation': 1.0,
 		    'enabled': True,
+		    },
+		'B': {
+		    'coupling': 'DC',
+		    'VRange': .05,
+		    'probeAttenuation': 1.0,
+		    'enabled': True,
+		    },
+		'C': {
+		    'coupling': 'DC',
+		    'VRange': 1.0,
+		    'probeAttenuation': 1.0,
+		    'enabled': False,
+		    },
+		'D': {
+		    'coupling': 'DC',
+		    'VRange': 1.0,
+		    'probeAttenuation': 1.0,
+		    'enabled': False,
 		    },
 
 	}
 	#first one used to work for single trigger
-	data_format = {'A': ['gnd']}#, 'B':['acc']} #'exc':1}}#'test_new_trig': 1
+	data_format = {'A': ['0', '1', '2', '3', '4', '5', '6'], 'B': ['0', '1', '2', '3', '4', '5', '6']}#['i0', 'i1', 'f1', 'f0']}
 
-
-	#data_format = {'A': ['gnd', 'exc'],}
 Device = CavityPico
-'''
-		'B':{
-		    'coupling': 'DC',
-		    'VRange': 5,
-		    'probeAttenuation': 1.0,
-		    'enabled': True
-		    }
-		    '''
+
--- pico/devices/picoscope/device_6000a.py
+++ pico/devices/picoscope/device_6000a.py
@@ -48,7 +48,7 @@ class Picoscope(DefaultDevice):
         	ps.setNoOfCaptures(self.picoscope_n_capture)
         	self.ps = ps
         	print('set to trigger')
-        	#self.ps.runBlock(pretrig=0.0, segmentIndex=0)
+        	self.ps.runBlock(pretrig=0.0, segmentIndex=0)
 		#self.ps.waitReady()
 		
 	def reset(self):
@@ -56,13 +56,71 @@ class Picoscope(DefaultDevice):
 		self.ps.runBlock(pretrig=0.0, segmentIndex=0)	
 		print('reset: ran block')
 		#self.ps.waitReady()
+	
+	def record(self, rel_data_path):
+		self.recordMultipleTriggers(rel_data_path)
 	'''
 	def set_max_V(self, V_new):
 		self.picoscope_channel_settings['A']['VRange'] = V_new
 		self.ps.setChannel('A', **self.picoscope_channel_settings['A'])
 		print('Pico channel A max voltage set to {} V'.format(V_new))	
 	'''	
-	def record(self, rel_data_path):
+	def recordMultipleTriggers(self, rel_data_path):
+		print('called!')
+		self.ps.waitReady()
+		print('ready')
+		t0 = time.time()
+    
+		print(self.data_format.items())
+		n_channels = len(self.data_format.items())
+		for ix_c in np.arange(n_channels):
+			channel, segments = self.data_format.items()[ix_c] #MM 20230313assuming for
+			(data_raw, numSamples, overflow) = self.ps.getDataRawBulk(channel=channel) #bulk for multiple triggers
+			print(data_raw.shape)
+			print(channel)
+			dataV = self.ps.rawToV(channel, data_raw)
+			print(np.shape(dataV))
+			data = {}
+			data[channel] = {}
+			for i in np.arange(len(segments)):
+				data[channel][segments[i]] = dataV[i, :]
+		
+			#Save data. Data file path comes from conductor parameter, where condition for temporarily or permanently saving data is set. 
+			#Check if today's data folder already exists; if not, make it
+			time_string = time.strftime('%Y%m%d')
+			dir_path = os.path.join(self.data_path, time_string)
+			if not os.path.isdir(dir_path):
+			    os.makedirs(dir_path)
+			
+			#Save data
+			raw_data = data[channel]
+			ts = np.arange(self.n_samples)*self.picoscope_sampling_interval
+			
+			#MM 20240118 modified to record data from multiple channels
+			path_end = '_{}.hdf5'.format(channel)
+			h5py_path = os.path.join(self.data_path, rel_data_path + path_end)
+			if ix_c == 0:
+				server_path = h5py_path
+			try:
+				h5f = h5py.File(h5py_path, 'w')
+				for k, v in raw_data.items():
+			    		h5f.create_dataset(k, data=np.array(v), compression='gzip')
+			    	h5f.create_dataset('time', data = np.array(ts), compression = 'gzip')
+				h5f.close()
+			except:
+				print('Unable to save pico file!')
+			
+        	print('data saved')
+        	tf = time.time()
+        	print("Elapsed time recording: {}".format(tf - t0))
+		message = {'record': {self.name: server_path}}
+		self.server._send_update(message)
+		
+		self.ps.stop()
+		self.ps.setNoOfCaptures(self.picoscope_n_capture)
+		self.ps.runBlock()
+		
+	def recordSingleTrigger(self, rel_data_path):
 		print('called!')
 		self.ps.waitReady()
 		print('ready')
