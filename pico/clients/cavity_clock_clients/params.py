9bd2fbd047a611512d64bdb7c0242a2586da8b87
sr3srgang <sr3srgang@gmail.com>
2024-11-11 22:40:26 -0700

    Updated PMT voltage integration range

--- pico/clients/cavity_clock_clients/params.py
+++ pico/clients/cavity_clock_clients/params.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 freq_offset = 116.55e6
-pico_shot_range = np.arange(7, 27)
+pico_shot_range = np.arange(1, 120)
 crossing_emp = .0027#-.0004
 t_range_emp = [.00325, .020]
 scan_rate_emp = 1e6/(20e-3) #1MHz/20 ms
