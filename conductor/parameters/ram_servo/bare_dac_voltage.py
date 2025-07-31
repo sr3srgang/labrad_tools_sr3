a9772bb0328fb04b2522e8862ab3cb1572326834
sr3srgang <sr3srgang@gmail.com>
2025-07-31 12:48:00 -0600

    added ram servo files

---
+++ conductor/parameters/ram_servo/bare_dac_voltage.py
@@ -0,0 +1,49 @@
+from conductor.parameter import ConductorParameter
+
+#MM 20250731
+#read in value of bare cavity dac offset from pico client
+#to be used with cav_aom_813_rigol in setting up a RAM servo for cavity lock
+class BareDACVoltage(ConductorParameter):
+    autostart = True
+    priority = 5 #just needs to be higher htan cav_aom_813_rigol
+   
+    data_filename = 'bare_dac_voltage_{}.txt'
+    data_directory = os.path.join(os.getenv('PROJECT_DATA_PATH'), 'data')
+
+    record_sequences = [
+    	'cav_sweep', 
+    	'cav_sweep_bare'	
+        ]
+        
+    def initialize(self, config):
+        super(BareDACVoltage, self).initialize(config)
+        self.connect_to_labrad()
+
+    def find_path(self):
+    	experiment_name = self.server.experiment.get('name')
+        shot_number = self.server.experiment.get('shot_number')
+        
+        sequence = self.server.parameters.get('sequencer.sequence')
+        previous_sequence = self.server.parameters.get('sequencer.previous_sequence')
+        
+        if (experiment_name is not None) and (sequence is not None):
+            point_filename = self.data_filename.format(shot_number)
+            rel_point_path = os.path.join(experiment_name, point_filename)
+        
+        if len(np.intersect1d(sequence.value, self.record_sequences)) > 0:# is not None:
+            return os.path.join(data_dirctory, rel_point_path)
+    	
+    def update(self):
+        path = self.find_path()
+        print(path)
+        if path is not None:
+            try:
+                with open(path, 'r') as file:
+                    val = file.read()
+                print(val)
+                self.value = val
+
+        #print(self.value)
+ 
+    
+Parameter = BareDACVoltage
---
+++ conductor/parameters/ram_servo/steer_cav.py
@@ -0,0 +1,52 @@
+import json
+import os
+import time
+from data_analysis.cavity_clock.read_data import *
+from data_analysis.cavity_clock.helpers import *
+from twisted.internet import reactor
+from conductor.parameter import ConductorParameter
+from data_analysis.PID import PID
+
+
+
+class RamServoLock(ConductorParameter):
+    autostart = False
+    priority = 10
+    data_directory = os.path.join(os.getenv('PROJECT_DATA_PATH'), 'data')
+    #data_filename = '{}.conductor.json'
+    call_in_thread = False
+    default = 102.160e6
+
+    def initialize(self, config):
+        super(RamServoLock, self).initialize(config)
+        self.connect_to_labrad()
+        
+        print("config:")
+        print(config)
+        active = config.get('servo_active')
+        self.active = active
+        if active:
+		default_val = config.get('script_default')
+		if default_val is not None:
+		    self.default = default_val
+		#TUNE THESE!!
+		self.PID_params = {"k_prop": -1, "t_int": 10, "t_diff": 0, "setpoint":.5,  "dt": 1, "output_default":self.default}
+		self.PID = PID(self.PID_params)
+		print('Turning on cav ram servo')
+        
+    def update(self):
+    	if self.active:
+		print('ram servo update at {}'.format(time.time()))
+		
+		err_sig = self.server.parameters.get('bare_dac_voltage').value
+		if err_sig is not None:
+		    out = self.PID.update(err_sig)
+		    print("new cav aom val from ram servo: {}".format(out)
+		    self.server.parameters.get('cav_aom_813_rigol').set_value_lock(out)
+		    
+        
+        
+        
+        
+    
+Parameter = RamServoLock
